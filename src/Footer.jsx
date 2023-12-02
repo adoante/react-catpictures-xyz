ba70f17b45a6eb50c38e74734a22d152323ff097
adoante <adolfogante@gmail.com>
2023-12-02 14:25:20 -0800

    styled button

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
 
 	return (
 		<footer>
+			<br></br>
 			Made by <a href="https://adolfogante.com">Adolfo Gante</a><br></br> CSS Provided by <a href="https://vanillacss.com/">Vanilla CSS</a>
 		</footer>
 	);
