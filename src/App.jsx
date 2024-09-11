90b9c80e7cb135188cc7d29529a2e5bebad2b2d0
Adolfo Gante <adolfogante@gmail.com>
2024-09-10 20:11:46 -0700

    fixed spacing, added styled <hr>

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ function App() {
       <div id="content">
         <h1>Cat Pictures</h1>
     	  <GetCatPic />
-        <h2> Advice from Cats </h2>
+        <hr></hr>
+        <h1> Advice from Cats </h1>
         <GetAdvice />
+        <hr></hr>
         <Footer />
       </div>
     </>
